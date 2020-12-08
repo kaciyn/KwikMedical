6a00f79be471310d515496f65c388f5a0a2ee79c
Kaci <25372015+kaciyn@users.noreply.github.com>
2020-12-08 05:08:14 +0100

    Db works to my surprise and delight, just had fucked the connection string

--- src/main/java/com/example/KwikMedical/Data/DatabaseHelper.java
+++ src/main/java/com/example/KwikMedical/Data/DatabaseHelper.java
@@ -15,14 +15,15 @@ public class DatabaseHelper
             // Load the driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             var localDatabaseUri="jdbc:mysql://localhost:3306/mydb";
-            var remoteDatabaseUri="jdbc:mysql://freedb.tech:3306/mydb";
+            var remoteDatabaseUri="jdbc:mysql://freedb.tech:3306/";
 
+            var db="freedbtech_KwikMedical";
             var user="freedbtech_kaci";
             var password="password";
 
             // First we need to establish a connection to the database
             return DriverManager
-                    .getConnection(remoteDatabaseUri, user, password);
+                    .getConnection(remoteDatabaseUri+db, user, password);
         }
         catch (ClassNotFoundException classNotFoundException) {
             System.err.println("Could not load driver");
